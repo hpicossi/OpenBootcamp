b0d6287e213bd58f99946eda8bee820032be1e42
Hilario <hpicossi@gmail.com>
2023-05-11 18:25:35 -0300

    Ejercicios 1 y 2 en program.cs

---
+++ C#/Practica/Practica/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Ejercicio 1
+
+Console.Write("Por favor ingrese su nombre: ");
+string nombre = Console.ReadLine();
+Console.WriteLine("Hola, " + nombre + "!");
+
+//Ejercicio 2
+
+DateTime horaActual = DateTime.Now;
+Console.WriteLine("La hora actual es: " + horaActual.ToString("hh:mm:ss tt"));
